77521021529ee501f12ffe5b134d7e6d4559e1ba
Aurelien Brabant <gh@aurelle.dev>
2026-08-23 00:04:20 +0200

    nix: add withRepl flag

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "numen: a zero dependency calculator library with first class support for units and timezone conversions";
+  description = "numen: natural language calculator library";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -7,37 +7,17 @@
     let
       lib = nixpkgs.lib;
       forAllSystems = f: lib.genAttrs lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
-      version = builtins.head (builtins.match ".*project\\([^)]*VERSION ([0-9.]+).*" (builtins.readFile ./CMakeLists.txt));
     in
     {
       packages = forAllSystems (pkgs: rec {
-        numen = pkgs.stdenv.mkDerivation {
-          pname = "numen";
-          inherit version;
-          src = self;
-
-          nativeBuildInputs = [ pkgs.cmake pkgs.ninja ];
-          buildInputs = [ pkgs.openssl ];
-          nativeCheckInputs = [ pkgs.catch2_3 ];
-
-          cmakeFlags = [
-            (lib.cmakeBool "BUILD_SHARED_LIBS" true)
-            (lib.cmakeBool "BUILD_REPL" true)
-            (lib.cmakeBool "BUILD_TESTS" true)
-            (lib.cmakeBool "USE_SYSTEM_CATCH" true)
-          ];
-
-          doCheck = true;
-
-          meta = {
-            description = "Zero dependency calculator library with first class support for units and timezone conversions";
-            homepage = "https://github.com/vicinaehq/numen";
-            mainProgram = "numen";
-            platforms = lib.platforms.unix;
-          };
-        };
+        # override knobs: withRepl, doCheck, stdenv
+        numen = pkgs.callPackage ./nix/package.nix { src = self; };
         default = numen;
       });
+
+      overlays.default = final: prev: {
+        numen = final.callPackage ./nix/package.nix { src = self; };
+      };
 
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
---
+++ nix/package.nix
@@ -0,0 +1,36 @@
+{
+  lib,
+  stdenv,
+  cmake,
+  ninja,
+  openssl,
+  catch2_3,
+  src ? lib.cleanSource ../.,
+  withRepl ? true,
+  doCheck ? true,
+}:
+stdenv.mkDerivation {
+  pname = "numen";
+  # single source of truth: project(... VERSION x.y.z) in CMakeLists.txt
+  version = builtins.head (builtins.match ".*project\\([^)]*VERSION ([0-9.]+).*" (builtins.readFile ../CMakeLists.txt));
+  inherit src;
+
+  nativeBuildInputs = [ cmake ninja ];
+  buildInputs = lib.optionals withRepl [ openssl ];
+  nativeCheckInputs = [ catch2_3 ];
+
+  cmakeFlags = [
+    (lib.cmakeBool "BUILD_SHARED_LIBS" true)
+    (lib.cmakeBool "BUILD_REPL" withRepl)
+    (lib.cmakeBool "BUILD_TESTS" doCheck)
+    (lib.cmakeBool "USE_SYSTEM_CATCH" true)
+  ];
+
+  inherit doCheck;
+
+  meta = {
+    description = "Natural language calculator library";
+    homepage = "https://github.com/vicinaehq/numen";
+    platforms = lib.platforms.unix;
+  } // lib.optionalAttrs withRepl { mainProgram = "numen"; };
+}
